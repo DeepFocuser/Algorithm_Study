17087a7ea357f57fa7567230b5e28723e9a347f0
DeepFocuser <rlawhdrhs27@gmail.com>
2022-02-09 02:21:10 +0900

    leetcode 117 update

--- with_jh/PopulatingNextRightPointerInEachNode2_leetcode117.py
+++ with_jh/PopulatingNextRightPointerInEachNode2_leetcode117.py
@@ -102,6 +102,7 @@ def str_to_tree(string = None, p=None, q=None):
                         
         return tree[0].pop(), p, q
 
+# 1. 직관적인 방법
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
 
@@ -118,10 +119,63 @@ class Solution:
             get_depth(root.left)
             get_depth(root.right)
 
-            for _,v in tree_val.items():
+            for _, v in tree_val.items():
                 for c in range(len(v)-1):
                     v[c].next = v[c+1]
         
+        return root
+
+# 1. 직관적인 방법
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+
+        if root:
+            tree_val = defaultdict(list)
+            tree_val[0] =[root]
+
+            def get_depth(root, i=1):
+                if root:
+                    tree_val[i].append(root)
+                    get_depth(root.left, i+1)
+                    get_depth(root.right, i+1)
+
+            get_depth(root.left)
+            get_depth(root.right)
+
+            for _, v in tree_val.items():
+                for c in range(len(v)-1):
+                    v[c].next = v[c+1]
+        
+        return root
+
+'''
+Follow-up:
+You may only use constant extra space.
+The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+'''
+# 2.  implicit stack space does not count as extra space for this problem! using bfs
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+
+        if root:
+            deq = deque([root])
+            while deq:
+                
+                depth=[]
+
+                for _ in range(len(deq)):
+                    node=deq.popleft() # 너비 우선 탐색
+                    depth.append(node)
+
+                    # 왼쪽부터 차례대로 넣기
+                    if node.left:
+                        deq.append(node.left)
+                    if node.right:
+                        deq.append(node.right)
+
+                for i in range(len(depth)-1):
+                    depth[i].next = depth[i+1] 
+
         return root
 
 if __name__ == "__main__":
