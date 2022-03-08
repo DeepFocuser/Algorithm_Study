e10d31406e38befadb0882d564656aad74d3dcbd
DeepFocuser <rlawhdrhs27@gmail.com>
2022-03-08 23:30:52 +0900

    update

--- programmers/graph_순위.py
+++ programmers/graph_순위.py
@@ -33,4 +33,30 @@ def solution(n, results):
     
     return answer
 
+# https://namu.wiki/w/%ED%94%8C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%9B%8C%EC%85%9C%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98
 # 2. 플로이드 와샬 사용해보기
+def solution(n, results):  
+    
+    # 이긴 놈은 1, 진놈은 -1 로 초기화 / 아무것도 아닌경우 0
+    states = [[0]*n for _ in range(n)]
+    for row, column in results:
+        states[row-1][column-1] = 1 # 이긴애
+        states[column-1][row-1] = -1 # 진애
+    
+    # 단순히 반복문을 3번 중첩시키기만 하면 되기 때문에 구현에 있어 크게 어려운 부분은 없다. 단, 유의하여야 할 점은 for문에서 가운데 노드(m)가 제일 위에 있어야 한다는 점이다. 어쨌든 매우 간결하다.
+    for m in range(n):
+        for s in range(n):
+            for e in range(n):
+                if s == e: # 대각선 제외
+                    continue
+                # s가 m을 이기고 m이 e를 이겼다면, s가 e를 이긴 것
+                if states[s][m] == 1 and states[m][e] == 1:
+                    states[s][e] = 1
+                # s가 m에게 지고, m이 e에게 졌다면, s가 e에게 진 것
+                if states[s][m] == -1 and states[m][e] == -1:
+                    states[s][e] = -1
+    answer = 0
+    for state in states:
+        if state.count(0) == 1:
+            answer+=1
+    return answer
